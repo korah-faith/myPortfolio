53fa126ca98f34e5e07e387fea5aee9f5e554e9c
korah-faith <149183846+korah-faith@users.noreply.github.com>
2024-06-23 11:07:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+// document.getElementById('contactForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
     
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     const message = document.getElementById('message').value;
+//     const formMessage = document.getElementById('formMessage');
+//     const prompt = document.querySelector("#pl")
+
+//     if(name && email && message) {
+//         formMessage.textContent = 'Thank you for your message, ' + name + '! I will get back to you shortly.';
+//         formMessage.style.color = 'green';
+//     } else {
+//         prompt.classList.add(".pl")
+
+        
+//         formMessage.style.color = 'red';
+
+//     }
+// });
+
+const submit = document.querySelector('.submit');
+submit.addEventListener("click", ()=>{
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
-    const formMessage = document.getElementById('formMessage');
+    const prompt = document.querySelector("#pl");
+    const promp = document.querySelector(".pl");
+
 
     if(name && email && message) {
         formMessage.textContent = 'Thank you for your message, ' + name + '! I will get back to you shortly.';
         formMessage.style.color = 'green';
     } else {
-        formMessage.textContent = 'Please fill out all fields.';
+        promp.textContent = prompt.classList.add("pl")
+        console.log("hello")
 
         
         formMessage.style.color = 'red';
 
     }
-});
+})
+
